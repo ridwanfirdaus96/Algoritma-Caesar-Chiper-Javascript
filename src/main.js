9561e898a42076698af330d8758efba7cc2b078f
Muhammad Ridwan Firdaus <va.rhenka@gmail.com>
2019-05-16 09:20:36 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,30 @@
+//check if letter is uppercase
+function isUpperCase(str) {
+    return str === str.toUpperCase();
+}
+
+//decipher the string
+let ceaserCipher = (str, key) => {
+  let decipher = '';
+
+  //decipher each letter
+  for(let i = 0; i < str.length; i++){
+
+    //if letter is uppercase then add uppercase letters
+    if(isUpperCase(str[i])){
+      decipher += String.fromCharCode((str.charCodeAt(i) + key - 65) % 26 + 65);
+    }else{
+      //else add lowercase letters
+      decipher += String.fromCharCode((str.charCodeAt(i) + key - 97) % 26 + 97);
+    }
+  }
+
+
+  return decipher;
+}
+
+var enkripsi1 = prompt("Tuliskan pesan ");
+alert("Hasil enkripsi adalah: " + ceaserCipher(enkripsi1, +1)+ "");
+
+var enkripsi2 = prompt("Tuliskan pesan enkripsi");
+alert("Hasil dekripsi dari chipertext adalah: " + ceaserCipher(enkripsi2, -1)+ "");
